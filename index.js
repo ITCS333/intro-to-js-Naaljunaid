ad056a010e597ac248b011d01d575dc9bd2195aa
Noor <98556402+Naaljunaid@users.noreply.github.com>
2026-04-06 02:54:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @returns {number} The sum of a and b.
  */
 function sum(a, b) {
-  // TODO: Implement this function.
+  return a + b;
 }
 
 /**
@@ -12,7 +12,7 @@ function sum(a, b) {
  * @returns {string} The reversed string.
  */
 function reverseString(str) {
-  // TODO: Implement this function.
+  return str.split("").reverse().join("");
 }
 
 /**
@@ -20,7 +20,17 @@ function reverseString(str) {
  * @returns {number} The largest number in the array. Return null if the array is empty.
  */
 function findLargest(numbers) {
-  // TODO: Implement this function.
+   if (numbers.length === 0) {
+    return null;
+}
+  let largest = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > largest) {
+      largest = numbers[i];
+    }
+  }
+
+  return largest;
 }
 
 /**
@@ -30,7 +40,10 @@ function findLargest(numbers) {
  * The check should be case-insensitive.
  */
 function isPalindrome(str) {
-  // TODO: Implement this function.
+ const lowerStr = str.toLowerCase();
+  const reversed = lowerStr.split("").reverse().join("");
+
+  return lowerStr === reversed;
 }
 
 /**
@@ -38,7 +51,15 @@ function isPalindrome(str) {
  * @returns {number[]} A new array containing only the even numbers from the original array.
  */
 function filterEvenNumbers(numbers) {
-  // TODO: Implement this function.
+  let result = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      result.push(numbers[i]);
+    }
+  }
+
+  return result;
 }
 
 
